f9a9cce11b461fc79612bb5a88545a46b53dc614
HaruP0N <kukiurie333@gmail.com>
2026-01-29 23:37:56 -0300

    Update route.ts

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getAuthFromCookie } from "@/lib/auth";
 
 export async function GET() {
-  const me = getAuthFromCookie();
+  const me = await getAuthFromCookie(); // ← Agregado await
   if (!me) return NextResponse.json({ error: "No auth" }, { status: 401 });
   return NextResponse.json({ name: me.name, email: me.email });
 }
